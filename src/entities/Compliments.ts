64a1655c38ee3f22138c594bca5fa6c22e3f6fad
bruno <brunosilva@catho.com>
2021-06-29 12:08:25 -0300

    feat(compliments-entity): [BR] create entity from compliments

---
+++ src/entities/Compliments.ts
@@ -0,0 +1,52 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  JoinColumn,
+  ManyToOne
+} from 'typeorm';
+import { Tags } from '../entities/Tags';
+import { Users } from '../entities/Users';
+import { v4 as uuid } from 'uuid';
+
+@Entity('compliments')
+export class Compliments {
+
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  user_sender: string;
+
+  @JoinColumn({name: 'user_sender'})
+  @ManyToOne(() => Users)
+  userSender: Users;
+
+  @Column()
+  user_receiver: string;
+
+  @JoinColumn({name: 'user_receiver'})
+  @ManyToOne(() => Users)
+  userReceiver: Users;
+
+  @Column()
+  tag_id: string;
+
+  @JoinColumn({name: 'tag_id'})
+  @ManyToOne(() => Tags)
+
+  tag: Tags;
+
+  @Column()
+  message: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
