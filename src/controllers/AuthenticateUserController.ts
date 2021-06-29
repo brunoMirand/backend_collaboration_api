ed543cb5411e7106db790dbed6f3472adf1adaaf
bruno <brunosilva@catho.com>
2021-06-29 11:59:32 -0300

    feat(): [BR] create a control of authenticate users - using jsonwebtoken dependecie

---
+++ src/controllers/AuthenticateUserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { AuthenticateUserService } from '../services/AuthenticateUserService';
+
+export class AuthenticateUserController {
+  async handle(request: Request, response: Response) {
+    const { email, password } = request.body;
+    const token = await (new AuthenticateUserService().execute({
+      email,
+      password
+    }));
+
+    return response.status(200).json({ token: token });
+  }
+}
