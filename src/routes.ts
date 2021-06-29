c239cf07ada02d6d8e7578411f5c9ce6b3fdfc67
bruno <brunosilva@catho.com>
2021-06-29 12:11:22 -0300

    feat(routes): [BR] create a new routes to user authenticate and register a compliments

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/CreateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 
 export const router = Router();
 
 router.get('/', ensureAdmin, (_, response) => {
-  response.json({'message': 'Collaboration Application'});
+  response.json({ 'message': 'Collaboration Application' });
 });
 
 router.post('/users', (new CreateUserController).handle);
 router.post('/tags', ensureAdmin, (new CreateTagController).handle);
+router.post('/compliments', (new CreateComplimentController).handle);
+router.post('/singin', (new AuthenticateUserController).handle);
