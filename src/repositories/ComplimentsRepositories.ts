e3b05ead10034411eac155509a75fe4bcf9c95b1
bruno <brunosilva@catho.com>
2021-06-29 12:08:45 -0300

    feat(compliments-repository): [BR] create repository from compliments

---
+++ src/repositories/ComplimentsRepositories.ts
@@ -0,0 +1,7 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { Compliments } from '../entities/Compliments';
+
+@EntityRepository(Compliments)
+export class ComplimentsRepositories extends Repository<Compliments> {
+
+}
