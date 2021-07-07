fe5adc2afe5f4b1356338f328eafab9a0d85ee78
bruno <brunosilva@catho.com>
2021-07-06 21:00:41 -0300

    feat(middlewares): [DF] add layer to ensure authenticantion in routes to create compliments and tags

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,16 @@ import { CreateUserController } from './controllers/CreateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
+import { ensureAuthenticate } from './middlewares/unsureAuthenticate';
 import { CreateComplimentController } from './controllers/CreateComplimentController';
 
 export const router = Router();
 
-router.get('/', ensureAdmin, (_, response) => {
+router.get('/', ensureAuthenticate, ensureAdmin, (_, response) => {
   response.json({ 'message': 'Collaboration Application' });
 });
 
 router.post('/users', (new CreateUserController).handle);
-router.post('/tags', ensureAdmin, (new CreateTagController).handle);
-router.post('/compliments', (new CreateComplimentController).handle);
+router.post('/tags', ensureAuthenticate, ensureAdmin, (new CreateTagController).handle);
+router.post('/compliments', ensureAuthenticate, ensureAdmin, (new CreateComplimentController).handle);
 router.post('/singin', (new AuthenticateUserController).handle);
