d80d68b9137dcdc92c804999a78f2b46ef59ed66
bruno <brunosilva@catho.com>
2021-07-06 21:10:40 -0300

    style(dotenv): [BR] remove config from dotevn

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,8 +1,6 @@
 import { getCustomRepository } from 'typeorm';
 import { UsersRepositories } from '../repositories/UsersRepositories';
 import { hash } from 'bcryptjs';
-import dotenv from 'dotenv';
-dotenv.config();
 
 interface IUserRequest {
   name: string,
