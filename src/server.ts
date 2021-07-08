790c0e48431e07e6cde6a1a8e6152ddb95f159e5
bruno <brunosilva@catho.com>
2021-07-07 23:10:23 -0300

    refactor(error-handler): [BR] isolate the logic handler the error

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import 'express-async-errors';
+import { errorHandler } from './middlewares/errorHandle';
 import cors from 'cors';
 import { router } from './routes';
 
@@ -14,21 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof Error) {
-      return response.status(400).json({
-        status: 400,
-        message: error.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: 500,
-      message: 'Internal Server Error',
-    });
-  }
-);
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`The application is running and is available on the port ${port}`)
